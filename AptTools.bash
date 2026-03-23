b6b38df6fdb082ab95966a7283128693dd564ca8
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-03-23 04:38:50 -0300

    Improving script

--- AptTools.bash
+++ AptTools.bash
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 function apt_download_install() {
+
+  # Updating repository and installing requirement
+  DEBIAN_FRONTEND=noninteractive apt update
+  DEBIAN_FRONTEND=noninteractive apt install -y curl
+
+  # Downloading and installing packages
   for download_url in "$@" ; do
     download_dir="/tmp/apt/download/$(date "+%d-%m-%Y_%H:%M:%S")"
     mkdir -p "$download_dir" || exit
     download_filename="echo ${download_url##*/}"
-    DEBIAN_FRONTEND=noninteractive apt update
-    DEBIAN_FRONTEND=noninteractive apt install -y curl
     curl -L "$download_url" --output "$download_filename"
     DEBIAN_FRONTEND=noninteractive apt install -y "./$download_filename"
   done
