16657577cf21f9c9a64c3da2676ef622885f16fb
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-08-30 00:53:12 -0300

    Atualizando opções de instalação para incluir Henrique-Desktop e Henrique-Laptop

--- Instalar.bash
+++ Instalar.bash
@@ -5,11 +5,12 @@ function_return_variable=
 function obter_opcao() {
   _opcao_selecionada=-1
   re='^[0-9]+$'
-  while ! [[ $_opcao_selecionada =~ $re ]] || [ "$_opcao_selecionada" -lt 0 ] || [ "$_opcao_selecionada" -gt 2 ]; do
+  while ! [[ $_opcao_selecionada =~ $re ]] || [ "$_opcao_selecionada" -lt 0 ] || [ "$_opcao_selecionada" -gt 3 ]; do
     echo "O que deseja fazer?"
     echo "0 - Sair"
     echo "1 - Instalar sistema HMCC-Server"
-    echo "2 - Instalar sistema Henrique-PC"
+    echo "2 - Instalar sistema Henrique-Desktop"
+    echo "3 - Instalar sistema Henrique-Laptop"
     read -r _opcao_selecionada
   done
 
@@ -26,6 +27,8 @@ while [ "$opcao_selecionada" -ne 0 ]; do
   if [ "$opcao_selecionada" -eq 1 ]; then
     bash ./Instalar_HMCC_Server.bash
   elif [ "$opcao_selecionada" -eq 2 ]; then
-    bash ./Instalar_Henrique_PC.bash
+    bash ./Instalar_Henrique-Desktop.bash
+  elif [ "$opcao_selecionada" -eq 3 ]; then
+    bash ./Instalar_Henrique-Laptop.bash
   fi
 done
