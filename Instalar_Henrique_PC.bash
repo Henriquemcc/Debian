19c39db141700ead8af73a5873c6f202bdac9943
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-09-05 17:48:45 -0300

    Adicionando instalação do Rancher Desktop

--- Instalar_Henrique_PC.bash
+++ Instalar_Henrique_PC.bash
@@ -282,6 +282,9 @@ function run_as_root() {
   # Instalando Kubernetes
   bash ./Install-Kubernetes.bash
 
+  # Instalando o Rancher Desktop
+  bash ./Install-RancherDesktop.bash
+
   # Instalando o VirtualBox
   bash ./Install-OracleVirtualBox.bash
   bash ./Sign-VirtualBox.bash
